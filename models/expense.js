301f9c827cc64f8f514be4a060f9f259b12db973
Shawn Kang <shawnkang0818@gmail.com>
2024-01-22 18:22:00 -0500

    change the variable amount to price , anc change the amount in expense list to price

--- models/expense.js
+++ models/expense.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const expenseSchema = new Schema({
-  amount: Number,
+  price: Number,
   date: Date,
   // category: [categorySchema],
   description: String,
