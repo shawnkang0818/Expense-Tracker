431ed402c03458d9988e0677b064e059be2b1cd0
Shawn Kang <shawnkang0818@gmail.com>
2024-01-24 23:45:41 -0500

    style expenses index and show view

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -1,5 +1,19 @@
 import { Budget } from "../models/budgets.js"
 
+function newBudget(req, res) {
+  Budget.find({}).sort('name')
+  .then(budget => {
+    res.render('budgets/new', {
+      title: 'Add Budget',
+      budget,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/budgets")
+  })
+}
+
 function create(req, res) {
   req.body.owner = req.user.profile._id
   Budget.create(req.body)
@@ -14,4 +28,5 @@ function create(req, res) {
 
 export {
   create,
+  newBudget,
 }
--- routes/budgets.js
+++ routes/budgets.js
@@ -6,7 +6,7 @@ import { isLoggedIn } from "../middleware/middleware.js";
 const router = Router()
 
 //GET localhost:3000/new
-router.get('/new', isLoggedIn,  budgetsCtrl.create)
+router.get('/new', isLoggedIn,  budgetsCtrl.newBudget)
 // POST localhost:3000/budgets
 router.post('/', isLoggedIn, budgetsCtrl.create);
 export {
