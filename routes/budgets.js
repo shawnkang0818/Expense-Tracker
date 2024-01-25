c6a4575922384f5cee830df41188a4d8e29712af
Shawn Kang <shawnkang0818@gmail.com>
2024-01-25 04:51:19 -0500

    add route to delete budget

--- routes/budgets.js
+++ routes/budgets.js
@@ -10,6 +10,8 @@ const router = Router()
 router.post('/', isLoggedIn, budgetsCtrl.create);
 // Get localhost:3000/budgets
 router.get('/', isLoggedIn, budgetsCtrl.index);
+//DETELE localhost:3000/budgets/:budgetId/
+router.delete('/:budgetId', isLoggedIn, budgetsCtrl.delete)
 export {
   router
 }
